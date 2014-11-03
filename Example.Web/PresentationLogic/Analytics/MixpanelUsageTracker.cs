e7226312b2b83adf3d6bdbc41fd600d7c6b09342
Martin H. Normark <m@martinnormark.com>
2014-11-04 00:39:32 +0100

    Add Mixpanel tracking script

---
+++ Example.Web/PresentationLogic/Analytics/MixpanelUsageTracker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Example.Web.PresentationLogic.Analytics
+{
+	public class MixpanelUsageTracker
+	{
+#if DEBUG
+		/// <summary>
+		/// Test site
+		/// </summary>
+		public const string Token = "TOKEN";
+#else
+		/// <summary>
+		/// Production site
+		/// </summary>
+		public const string Token = "TOKEN";
+#endif
+	}
+}
